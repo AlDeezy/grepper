60e4262de009fb834fbc3c4b992157af21ebc7c2
alex miller <amiller@porthas.inc>
2022-05-10 17:23:44 -0400

    Progress, some personal comments

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ fn main() {
     // Warning: this will panic if we aren't using valid Unicode characters!
     let args: Vec<String> = env::args().collect();
     
+    // Because Config::new returns a Config instance, we use unwrap_or_else
+    // This is because we want to assign config a Config instance,
+    //   or we want to handle any error that occurs in this process
     let config = Config::new(&args).unwrap_or_else(|err|{
         println!("Problem parsing args: {}", err);
         process::exit(1);
@@ -15,6 +18,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In {}", config.filename);
 
+    // Because we only care whether run returns an error, we don't need
+    //   to unwrap it, thus we use this 'if let' pattern
+    // run only returns the unit "()" type on success
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
         process::exit(1);
@@ -27,6 +33,8 @@ struct Config {
 }
 
 impl Config {
+    // Takes in a pointer to a String array (ie, the arguments passed within the cli)
+    // Returns a new Config instance upon success, or an Err(&str)
     fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Not enough arguments!");
